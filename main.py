a45b5f1ef6585b4922ab85b65d210fd5919daf54
Tamerlan13101975 <166745761+Tamerlan13101975@users.noreply.github.com>
2024-05-03 17:00:56 +0300

    add conditions code

--- main.py
+++ main.py
@@ -2,4 +2,16 @@ class Student:
     def __init__(self, name, surname):
         self.name = name
         self.surname = surname
-        self.grades = []
+        self.grades = [0]
+
+    def add_grade(self, grade):
+        self.grades.append(grade)
+
+    def calculate_average_grade(self):
+        if len(self.grades) == 0:
+            return 0.0
+        else:
+            return sum(self.grades) / len(self.grades)
+
+    def __str__(self):
+        return f"Студент: {self.name} {self.surname}nОценки: {self.grades}nСредний балл: {self.calculate_average_grade()}"
